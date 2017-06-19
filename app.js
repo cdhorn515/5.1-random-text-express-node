d703acf0551cc578a62f41f00a906f35eb728ef4
Christina Cornell <cdhorn515@gmail.com>
2017-06-19 16:01:45 -0400

    finished assessment

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var loremIpsum = require('lorem-ipsum');
 
 const app = express();
 
-//create variable with options for text
+// create variable with options for text
 var output = loremIpsum({
   count: 3,
   units: "paragraphs",
@@ -13,10 +13,22 @@ var output = loremIpsum({
   format: 'html'
 });
 
-//
-app.get('/lorem/', function(req, res) {
+
+app.get('/lorem', function(req, res) {
   res.send(output);
 });
+
+// app.get('/lorem/:count', function(req, res) {
+//   res.send(loremIpsum({
+//     count: req.params.count,
+//     units: "paragraphs",
+//     paragraphLowerBound: 4,
+//     paragraphUpperBound: 7,
+//     format: 'html'
+//   });
+// });
+// });
+
 
 app.listen(3000, function(){
   console.log("Express app listening for connection");
